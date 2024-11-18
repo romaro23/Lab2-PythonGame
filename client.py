da193be1b11b80e3326ecbe1fb8343c7e5c73259
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2024-11-18 19:38:47 +0200

    Save

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 import socket
+import time
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 from random import randint
 import threading
+import time
 
 
 class GameClient:
@@ -22,52 +25,87 @@ class GameClient:
         self.time_left = 0
         self.other_player_score = 0
         self.is_running = False
+        self.is_tournament = False
+        self.tournament_score = []
+        self.tournament_other_player_score = []
+        self.summary = 0
+        self.is_other_ready = False
+        self.tour_number = 0
         self.check_thread = threading.Thread(target=self.check, daemon=True)
         self.check_thread.start()
         self.setup_gui()
 
     def setup_gui(self):
         self.root = tk.Tk()
-        self.root.title("Player1")
-
-        self.canvas = tk.Canvas(self.root, width=800, height=600)
-        self.canvas.pack()
+        time.sleep(1)
+        self.root.title(f"Player {self.player_id}")
+        self.root.resizable(False, False)
+        self.canvas = tk.Canvas(self.root, width=800, height=400)
+        self.canvas.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
         self.timer_label = tk.Label(self.root, text=f"Time left: {self.time_left}", font=("Arial", 24))
-        self.timer_label.pack()
-        self.score_label = tk.Label(self.root, text=f"Your score: {self.score}",
-                                    font=("Arial", 24))
-        self.other_player_score_label = tk.Label(self.root, text=f"Other player's score: {self.other_player_score}",
-                                                 font=("Arial", 24))
+        self.timer_label.grid(row=1, column=1, pady=5)
+        self.score_label = tk.Label(self.root, text=f"Your score: {self.score}", font=("Arial", 24))
+        self.score_label.grid(row=1, column=0, padx=5, pady=5)
+        self.other_player_score_label = tk.Label(
+            self.root, text=f"Other player's score: {self.other_player_score}", font=("Arial", 24)
+        )
+        self.other_player_score_label.grid(row=2, column=0, padx=5, pady=5)
         self.spinbox = tk.Spinbox(self.root, from_=2, to=100)
-        self.spinbox.pack()
+        self.spinbox.grid(row=2, column=1, padx=5, pady=5)
+        if self.player_id != 1:
+            self.spinbox.config(state="disabled")
         self.start_button = tk.Button(self.root, text="Start Game", command=self.send_ready)
-        self.start_button.pack()
+        self.start_button.grid(row=1, column=2, padx=5, pady=5)
+        self.tournament_button = tk.Button(self.root, text="Start tournament", command=self.start_tournament)
+        self.tournament_button.grid(row=2, column=2, padx=5, pady=5)
         self.restart_button = tk.Button(self.root, text="Restart", command=self.restart)
-        self.restart_button.pack()
-        self.other_player_score_label.pack()
-        self.score_label.pack()
+        self.restart_button.grid(row=3, column=2, padx=5, pady=5)
         self.canvas.bind('<Motion>', self.check_mouse_position)
-
         self.root.mainloop()
+
     def send_ready(self):
-        self.client_socket.send(b"PLAYER1_READY")
-    def start_game(self):
-        self.is_running = True
-        self.time_left = int(int(self.spinbox.get()) * 0.6)
-        self.generate_circles()
-        self.start_timer()
-        self.spinbox.config(state="disabled")
-        self.start_button.config(state="disabled")
+        message = f"Player {self.player_id} ready"
+        self.client_socket.send(message.encode())
+        self.send_circles()
+
+    def send_ready_round(self):
+        message = f"Player ready for round"
+        self.client_socket.send(message.encode())
+
+    def send_ready_tour(self):
+        message = f"Player {self.player_id} ready for tournament"
+        self.client_socket.send(message.encode())
+        self.send_circles()
+
+    def send_score(self):
+        score_message = self.score.__str__()
+        self.client_socket.send(score_message.encode())
+
+    def send_summary(self):
+        for s in self.tournament_score:
+            self.summary += s
+        message = f"Summary of PLAYER{self.player_id}: " + self.summary.__str__()
+        print(message)
+        self.client_socket.send(message.encode())
+
+    def send_circles(self):
+        if self.player_id == 1:
+            message = f"Circles: {self.spinbox.get()}"
+            self.client_socket.send(message.encode())
 
     def generate_circles(self):
         n = int(self.spinbox.get())
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
         for _ in range(n):
-            x, y = randint(50, 750), randint(50, 550)
+            x, y = randint(20, canvas_width - 20), randint(20, canvas_height - 20)
             circle = self.canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill='blue')
             self.active_circles.append(circle)
+
     def clear_circles(self):
         for _ in range(len(self.active_circles)):
             self.canvas.delete(self.active_circles.pop())
+
     def check_mouse_position(self, event):
         if not self.is_running:
             return
@@ -80,14 +118,11 @@ class GameClient:
                     self.score_label.config(text=f"Your score: {self.score}")
                     self.send_score()
                     if self.score == len(self.active_circles):
-                        print("You've won")
-                        self.client_socket.send(b"PLAYER_WON")
                         self.end_game()
-                        #messagebox.showinfo("", "You have won")
-
-    def send_score(self):
-        score_message = self.score.__str__()
-        self.client_socket.send(score_message.encode())
+                        if not self.is_tournament:
+                            print("You've won")
+                            self.client_socket.send(b"PLAYER_WON")
+                            messagebox.showinfo("", "You have won")
 
     def check(self):
         while True:
@@ -96,45 +131,84 @@ class GameClient:
                 if not data:
                     break
                 message = data.decode()
-                print(message)
                 if message == "StartGame":
                     if not self.is_running:
-                        print("Starting game...")
                         self.start_game()
+                elif message == "StartTournament":
+                    if not self.is_tournament:
+                        self.is_tournament = True
+                        self.show_table_messagebox()
                 elif message == "PLAYER_WON":
-                    print("You've lost")
-                    self.end_game()
-                    #messagebox.showinfo("", "You have lost")
+                    if not self.is_tournament:
+                        print("You've lost")
+                        self.end_game()
+                        messagebox.showinfo("", "You have lost")
                 elif message.startswith("PLAYER_ID:"):
                     self.player_id = int(message.split(":")[1])
                     print(f"Assigned Player ID: {self.player_id}")
+                elif message == "Other player ready":
+                    self.is_other_ready = True
+                elif message == "You've won":
+                    messagebox.showinfo("", "You have won the tournament")
+                    break
+                elif message == "You've lost":
+                    messagebox.showinfo("", "You have lost the tournament")
+                    break
+                elif message == "Draw":
+                    messagebox.showinfo("", "It is draw. There is no winner")
+                    break
                 elif message.startswith("Player "):
                     player_id, score = message.split(" ")[1], message.split(": ")[1]
                     if int(player_id) != self.player_id:
                         self.other_player_score = int(score)
                         self.other_player_score_label.config(
                             text=f"Other player's score: {self.other_player_score}")
-            except Exception as e:
-                print(f"Помилка: {e}")
-                break
-
-    # def receive_scores(self):
-    #     while True:
-    #         try:
-    #             data = self.client_socket.recv(1024).decode()
-    #             if data.startswith("PLAYER_ID:"):
-    #                 self.player_id = int(data.split(":")[1])
-    #                 print(f"Assigned Player ID: {self.player_id}")
-    #             elif data.startswith("Player "):
-    #                 player_id, score = data.split(" ")[1], data.split(": ")[1]
-    #                 if int(player_id) != self.player_id:
-    #                     self.other_player_score = int(score)
-    #                     self.other_player_score_label.config(
-    #                         text=f"Other player's score: {self.other_player_score}")
+                elif message.startswith("Circles"):
+                    circles = message.split(":")[1]
+                    self.spinbox.config(state="normal")
+                    self.spinbox.delete(0, "end")
+                    self.spinbox.insert(0, circles)
+                    self.spinbox.config(state="disabled")
 
             except Exception as e:
                 print(f"Помилка: {e}")
                 break
+
+    def show_table_messagebox(self):
+        table_window = tk.Toplevel()
+        table_window.title("Results")
+        table_window.geometry("300x150")
+
+        table = ttk.Treeview(table_window, columns=("Your score", "Other player's score"), show="headings", height=3)
+        table.heading("Your score", text="Your score")
+        table.heading("Other player's score", text="Other player's score")
+        table.column("Your score", width=120, anchor="center")
+        table.column("Other player's score", width=120, anchor="center")
+        table.pack(padx=10, pady=10)
+
+        def update_table():
+            for row in table.get_children():
+                table.delete(row)
+            for p1, p2 in zip(self.tournament_score, self.tournament_other_player_score):
+                table.insert("", "end", values=(p1, p2))
+
+        update_table()
+
+        timer_label = tk.Label(table_window, text="Закроется через 5 секунд")
+        timer_label.pack(pady=5)
+
+        def update_timer(seconds_left):
+            if seconds_left > 0:
+                timer_label.config(text=f"Time to game: {seconds_left}")
+                table_window.after(1000, update_timer, seconds_left - 1)
+            else:
+                if self.tour_number != 3:
+                    self.start_game()
+                    table_window.destroy()
+                else:
+                    timer_label.pack_forget()
+
+        update_timer(2)
 
     def start_timer(self):
         if self.time_left > 0:
@@ -148,18 +222,65 @@ class GameClient:
         if hasattr(self, 'timer_event'):
             self.root.after_cancel(self.timer_event)
             del self.timer_event
+
+    def start_tournament(self):
+        self.send_ready_tour()
+
+    def start_game(self):
+        self.is_running = True
+        self.time_left = int(int(self.spinbox.get()) * 0.6)
+        self.generate_circles()
+        self.start_timer()
+        self.spinbox.config(state="disabled")
+        self.start_button.config(state="disabled")
+
+    def wait_for_other_player_ready(self):
+        if self.is_other_ready:
+            self.handle_tournament_round()
+        else:
+            self.root.after(1000, self.wait_for_other_player_ready)
+
+    def handle_tournament_round(self):
+        if self.tour_number < 3:
+            self.tournament_score.append(self.score)
+            self.tournament_other_player_score.append(self.other_player_score)
+            self.show_table_messagebox()
+            self.clear_circles()
+            self.score = 0
+            self.other_player_score = 0
+            self.tour_number += 1
+            self.is_other_ready = False
+
     def end_game(self):
         self.is_running = False
         self.stop_timer()
+        if self.is_tournament and self.tour_number != 2:
+            self.send_ready_round()
+            self.wait_for_other_player_ready()
+        else:
+            self.send_summary()
+            self.summary = 0
+
         # if self.client_socket:
         #     self.client_socket.send(b"GAME_OVER")
         # if self.client_socket:
         #     self.client_socket.close()
+
     def restart(self):
-        self.spinbox.config(state="normal")
+        self.end_game()
+        if self.player_id == 1:
+            self.spinbox.config(state="normal")
         self.start_button.config(state="normal")
         self.clear_circles()
         self.score = 0
         self.other_player_score = 0
+        self.stop_timer()
+        self.tour_number = 0
+        self.tournament_score.clear()
+        self.tournament_other_player_score.clear()
+        self.is_other_ready = False
+        self.is_tournament = False
+
+
 if __name__ == "__main__":
     client = GameClient()
--- server.py
+++ server.py
@@ -16,49 +16,81 @@ class GameServer:
         self.server_socket.listen()
         self.PLAYER1 = False
         self.PLAYER2 = False
+        self.PLAYER1_TOUR = False
+        self.PLAYER2_TOUR = False
+        self.PLAYER1_SUM = False
+        self.PLAYER2_SUM = False
+        self.SUM1 = 0
+        self.SUM2 = 0
         print(f"Server started on {host}:{port}")
-        self.clients = {}  # Зберігаємо клієнтів та їхні рахунки
+        self.clients = {}
 
         while True:
             client_socket, addr = self.server_socket.accept()
             print(f"Connection from {addr}")
-            player_id = len(self.clients) + 1  # Присвоюємо кожному клієнту унікальний ID
+            player_id = len(self.clients) + 1
             self.clients[client_socket] = {'score': 0, 'id': player_id}
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
             threading.Thread(target=self.start_game).start()
+            threading.Thread(target=self.start_tournament).start()
+            threading.Thread(target=self.end_tournament).start()
+
     def start_game(self):
         while True:
             if self.PLAYER1 and self.PLAYER2:
-                print("Starting game...")
                 for client_socket in self.clients:
                     client_socket.send("StartGame".encode())
                     self.PLAYER1 = False
                     self.PLAYER2 = False
 
+    def end_tournament(self):
+        while True:
+            if self.PLAYER1_SUM and self.PLAYER2_SUM:
+                self.choose_tournament_winner()
+                self.PLAYER1_SUM = False
+                self.PLAYER2_SUM = False
 
-
+    def start_tournament(self):
+        while True:
+            if self.PLAYER1_TOUR and self.PLAYER2_TOUR:
+                for client_socket in self.clients:
+                    client_socket.send("StartTournament".encode())
+                    self.PLAYER1_TOUR = False
+                    self.PLAYER2_TOUR = False
 
     def handle_client(self, client_socket):
         player_id = self.clients[client_socket]['id']
-        client_socket.send(f"PLAYER_ID:{player_id}".encode())  # Відправляємо ID гравцю
-
+        client_socket.send(f"PLAYER_ID:{player_id}".encode())
         while True:
             try:
                 data = client_socket.recv(1024)
                 if not data:
                     break
                 message = data.decode()
-
                 if message == "PLAYER_WON":
-                    print("Player has won")
                     self.choose_winner(client_socket)
                 elif message.isdigit():
                     self.clients[client_socket]['score'] = int(message)
                     self.broadcast_scores(client_socket)
-                elif message == "PLAYER1_READY":
+                elif message == "Player 1 ready":
                     self.PLAYER1 = True
-                elif message == "PLAYER2_READY":
+                elif message == "Player 2 ready":
                     self.PLAYER2 = True
+                elif message == "Player 1 ready for tournament":
+                    self.PLAYER1_TOUR = True
+                elif message == "Player 2 ready for tournament":
+                    self.PLAYER2_TOUR = True
+                elif message == "Player ready for round":
+                    self.other_ready(client_socket)
+                elif message.startswith("Summary of PLAYER1"):
+                    self.SUM1 = message.split(":")[1]
+                    self.PLAYER1_SUM = True
+                elif message.startswith("Summary of PLAYER2"):
+                    self.SUM2 = message.split(":")[1]
+                    self.PLAYER2_SUM = True
+                elif message.startswith("Circles"):
+                    circles = message.split(":")[1]
+                    self.send_circles(client_socket, circles)
                 else:
                     print(f"Received unknown message from Player {player_id}: {message}")
 
@@ -69,18 +101,52 @@ class GameServer:
         # client_socket.close()
         # del self.clients[client_socket]  # Видаляємо клієнта зі списку
         # print(f"Connection closed for Player {player_id}")
+
     def choose_winner(self, sender_socket):
         for client_socket in self.clients:
             if client_socket != sender_socket:
                 message = "PLAYER_WON"
                 client_socket.send(message.encode())
+
+    def choose_tournament_winner(self):
+        if self.SUM1 > self.SUM2:
+            winner_id = 1
+        elif self.SUM2 > self.SUM1:
+            winner_id = 2
+        else:
+            winner_id = None
+        print("Some")
+        if winner_id is not None:
+            for client_socket, client_info in self.clients.items():
+                if client_info['id'] == winner_id:
+                    message = "You've won"
+                    client_socket.send(message.encode())
+                else:
+                    message = "You've lost"
+                    client_socket.send(message.encode())
+        else:
+            for client_socket in self.clients:
+                client_socket.send("Draw".encode())
+    def other_ready(self, sender_socket):
+        for client_socket in self.clients:
+            if client_socket != sender_socket:
+                message = "Other player ready"
+                client_socket.send(message.encode())
+
+    def send_circles(self, sender_socket, circles):
+        for client_socket in self.clients:
+            if client_socket != sender_socket:
+                message = f"Circles: {circles}"
+                client_socket.send(message.encode())
+
     def broadcast_scores(self, sender_socket):
         sender_id = self.clients[sender_socket]['id']
         sender_score = self.clients[sender_socket]['score']
         for client_socket in self.clients:
             if client_socket != sender_socket:
                 message = f"Player {sender_id} score: {sender_score}"
-                client_socket.send(message.encode())  # Відправляємо рахунок іншому гравцю
+                client_socket.send(message.encode())
+
 
 if __name__ == "__main__":
     GameServer()
